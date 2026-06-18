598715759b956bd06fc2297785d68dc6c44e809d
amctech106 <121874402+amctech106@users.noreply.github.com>
2026-06-18 22:28:10 +0500

    practice

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// console.log(Math.round(5.1));
+// console.log((5.18765432).toFixed(2));
+// console.log((5).toFixed(2));
+
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.9));
+
+// console.log(Math.abs(-6)); // sab value ko positive kardeta hy
+
+// let dice = Math.floor(Math.random() * 6);
+
+// console.log(dice + 1); // gives any random value between 0 to 1
+
+
+// console.log(Math.round(5.2))
+// console.log((5.1865423).toFixed(2))
+// console.log((5).toFixed(3))
