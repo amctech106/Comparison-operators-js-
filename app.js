e61d2faa94049a7523c38df109f50b79b7881b29
amctech106 <121874402+amctech106@users.noreply.github.com>
2026-06-19 22:55:25 +0500

    comparison Operators

--- app.js
+++ app.js
@@ -16,3 +16,57 @@
 // console.log(Math.round(5.2))
 // console.log((5.1865423).toFixed(2))
 // console.log((5).toFixed(3))
+
+let age = 15
+let requiredAge = 18
+
+console.log(age > requiredAge)
+console.log(age >= requiredAge)
+console.log(age <= requiredAge)
+console.log(age == requiredAge)
+console.log(age === requiredAge)
+console.log(age != requiredAge)
+console.log(age !== requiredAge)
+// <
+// >=
+// <=
+// ==
+// ===
+// !=
+// !==
+
+console.log(0 == false);
+console.log(0 === false);
+
+//🧠 Quiz 1
+
+console.log(5 == "5");
+console.log(5 === "5");
+
+//🧠 Quiz 2
+
+console.log(0 == false);
+console.log(0 === false);
+
+//🧠 Quiz 3
+
+console.log(null == undefined);
+console.log(null === undefined);
+
+//🧠 Quiz 4
+
+console.log("10" != 10);
+console.log("10" !== 10);
+
+//🧠 Quiz 5
+
+console.log(true == 1);
+console.log(true === 1);
+
+//🔥 Challenge (Bonus)
+
+let a = "0";
+let b = false;
+
+console.log(a == b);
+console.log(a === b);
